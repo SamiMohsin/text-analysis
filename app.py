ba3da777a493e8f618abd80fe8a7feb790a15a86
SamiMohsin <admin@graphicalinfo.com>
2019-10-11 11:30:10 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,24 @@
+'''
+The MIT License (MIT)
+
+Copyright (c) 2019 Ayoob Alshahri
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+software and associated documentation files (the "Software"), to deal in the Software 
+without restriction, including without limitation the rights to use, copy, modify, merge, 
+publish, distribute, sublicense, and/or sell copies of the Software, 
+and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all 
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO 
+THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
+OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+'''
+
 # import requests
 import requests
 
@@ -12,14 +33,21 @@ import lxml
 # alwatan.com الى http://alwatan.com 
 domain_regx = '(?: |//|^)([A-Za-z0-9]{1,}\.[A-Za-z0-9]{1,10}\.?[A-Za-z]{1,}\.?[A-Za-z]{1,})(?: |/|$)'
 
+sites = []
+
+'''
+
+فقط كمثال
+
 sites = [
- 'http://alwatan.com/',
+'http://alwatan.com/',
 'https://www.shabiba.com/',
 'https://www.omandaily.om/',
  'https://alroya.om/']
+ 
+'''
 
-# save files names
-# we want to be able to open file
+# الخطوه الاولى حفظ البيانات الى ملف من امتداد تكست
 files = []
 for ul in sites:
     raw = requests.get(ul)
@@ -34,19 +62,30 @@ for ul in sites:
             
             # most tags with text
             for row in soup.findAll({'h2','h3', 'p'، 'a'}):
+                # قم باضافتها الى المصفوفه تيكست
                 text.append(row.text)
-                a_writer.write(row.text)
+                #   يجب تحويل المصوفه الى من هيكل بياني الى نوع يسمى سترينج او نوع مدخلات رسوميه
+                # ، الحل اسفل هذا الكود  مباشره
+                #a_writer.write(row.text)
+                # تحويل المصفوفه الى سترينج
             str1 = ''.join(str(e) for e in text)
+            # قم بكتابه البيانات الى الملف 
             a_writer.write(str1)
             print(text)
         except Exception as err:
+           # في حاله ظهور خطأ قم باظهار نوع الخطأ
             print(err.args)
         finally:
+         # تاكد من اغلاق الامر رايتر
             a_writer.close()
 
 
 
-
+# قراه ملف التكست
+# لتفعيل ذلك يجب ان تقوم بصنع مصفوفه كمثال لذلك 
+# example = []
+# اكتب اسم الملف ثم اضف المصفوفه التس قمت بعملها كمثال ذلك 
+# output = read_raw_text('data/example.txt', example)
 def read_raw_text(file, dic):
     with open(file, 'r') as reader:
         try:
